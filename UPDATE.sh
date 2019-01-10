9d729bf510b35d33f920ec6b516a6656ba8cc479
pascalaebersold <46541784+pascalaebersold@users.noreply.github.com>
2019-01-10 21:36:44 +0100

    Update UPDATE.sh

--- UPDATE.sh
+++ UPDATE.sh
@@ -1,6 +1,8 @@
 !#/bin/bash
+sudo service apache2 stop
+sudo rm -r /home/pi/html
 sudo git clone https://github.com/pascalaebersold/html.git
 sudo rm -r /var/www/html/
 sudo cp -R /home/pi/html/ /var/www/
-sudo rm -r /home/pi/html
-sudo service apache2 restart
+sudo chmod u+x /var/www/html/UPDATE.sh
+sudo service apache2 start
