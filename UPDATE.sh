0e381213d40388f9863f53ea392801ee9d8a0b9c
pascalaebersold <46541784+pascalaebersold@users.noreply.github.com>
2019-01-10 21:04:46 +0100

    Add files via upload

--- UPDATE.sh
+++ UPDATE.sh
@@ -1,3 +1,6 @@
 !#/bin/bash
 sudo git clone https://github.com/pascalaebersold/html.git
-sudo sudo cp -R /home/pi/html/ /var/www/
+sudo rm -r /var/www/html/
+sudo cp -R /home/pi/html/ /var/www/
+sudo rm -r /home/pi/html
+sudo service apache2 restart
