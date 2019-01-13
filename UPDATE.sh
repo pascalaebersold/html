2ceda5b41ec02af071746babb4d4404390be3077
pascalaebersold <46541784+pascalaebersold@users.noreply.github.com>
2019-01-13 14:01:50 +0100

    Update UPDATE.sh

--- UPDATE.sh
+++ UPDATE.sh
@@ -1,3 +1,6 @@
 !#/bin/bash
-sudo git clone https://github.com/pascalaebersold/html.git /var/www/html
+sudo git clone https://github.com/pascalaebersold/html.git /var/www/htmlnew
+sudo mv /var/www/html /var/www/htmlold
+sudo mv /var/www/htmlnew /var/www/html
+sudo rm -r /var/www/htmlold
 sudo chmod u+x /var/www/html/UPDATE.sh
