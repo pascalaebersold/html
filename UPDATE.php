d64dae4c59f57275751b477dc90a3ebc81f7a73f
pascalaebersold <46541784+pascalaebersold@users.noreply.github.com>
2019-01-13 14:20:34 +0100

    Update UPDATE.php

--- UPDATE.php
+++ UPDATE.php
@@ -1,4 +1,6 @@
 <?php
 shell_exec("sudo /var/www/html/UPDATE.sh");
+shell_exec("DISPLAY=:0 xdotool search --desktop 0 'chromium-browser' windowactivate key Ctrl+1");
+shell_exec("DISPLAY=:0 xdotool search --desktop 0 'chromium-browser' windowactivate key Ctrl+F5");
 echo "<script>window.close();</script>";
 ?>
