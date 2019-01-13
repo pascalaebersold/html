3415f0319357262b6dafa6902ffdfcadce79b0d2
pascalaebersold <46541784+pascalaebersold@users.noreply.github.com>
2019-01-13 14:33:31 +0100

    Update EXIT.php

--- EXIT.php
+++ EXIT.php
@@ -1,4 +1,4 @@
 <?php
-shell_exec("DISPLAY=:0 xdotool search --desktop 0 'chromium-browser' windowactivate key F4");
+shell_exec("DISPLAY=:0 xdotool search --desktop 0 'chromium-browser' windowactivate key Alt+F4");
 echo "<script>window.close();</script>";
 ?>
