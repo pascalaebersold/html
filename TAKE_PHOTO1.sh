63beb5b624039f22a9083e56fa1d775ed380ba66
pascalaebersold <46541784+pascalaebersold@users.noreply.github.com>
2019-01-13 14:46:57 +0100

    Update TAKE_PHOTO1.sh

--- TAKE_PHOTO1.sh
+++ TAKE_PHOTO1.sh
@@ -2,5 +2,5 @@
 sudo killall raspistill
 DISPLAY=:0 xdotool search --desktop 0 'chromium-browser' windowactivate key Ctrl+1
 DATE=$(date +"%Y-%m-%d_%H%M")
-raspistill -p 0,0,800,350 -t 500 -o /var/www/html/pictures/$DATE.jpg
+sudo raspistill -p 0,0,800,350 -t 500 -o /var/www/html/pictures/$DATE.jpg
 raspistill -t 0 -p 0,0,800,350
